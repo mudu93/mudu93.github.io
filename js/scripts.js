20664979d600801b7f93eb0a742f9966485d315e
mudu sugal <msugal@hotmail.com>
2019-09-06 19:48:27 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 /*!
     Title: Dev Portfolio Template
     Version: 1.2.1
-    Last Change: 08/27/2017
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
 */
